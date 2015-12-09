f1d4e7e5b76215c5877fead4027affe4e2ea9e0f
Félicien François <felicien@tweakstyle.com>
2015-12-09 16:23:29 +0100

    added test

---
+++ test/test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var variableReplacer = require("../lib/variable-replacer.js");
+
+process.chdir(__dirname);
+
+variableReplacer({
+    source: 'input.txt',
+    loglevel: 'debug',
+    dest: 'output.txt',
+    dataSource: 'data.json',
+    inlineData: {
+        var2 : {
+        	key1: 'val2'
+        }
+    }
+});
